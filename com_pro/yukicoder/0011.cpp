1e5422b9e39a6d674bb6477036a9e18f8ea5b110
nobu <pyomta13@gmail.com>
2016-01-13 18:51:54 +0900

    Create 0011.cpp

---
+++ com_pro/yukicoder/0011.cpp
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <limits>
+#include <functional>
+#include <stack>
+#include <array>
+#include <fstream>
+#include <deque>
+#include <bitset>
+
+
+#define REP(i,n) for(int (i) = 0;(i) < (n) ; ++(i))
+#define REPS(a,i,n) for(int (i) = (a) ; (i) < (n) ; ++(i))
+#if defined(_MSC_VER)||__cplusplus > 199711L
+#define AUTO(r,v) auto r = (v)
+#else
+#define AUTO(r,v) typeof(v) r = (v)
+#endif
+#define ALL(c) (c).begin() , (c).end()
+#define EACH(it,c) for(AUTO(it,(c).begin());it != (c).end();++it)
+#define LL long long
+#define int LL
+#define inf  ((int)1 << 54)
+#define DIV 1000000007
+#define QUICK_CIN ios::sync_with_stdio(false); cin.tie(0);
+#define InitArr1(c,n) memset(&c[0],0,sizeof(int)*n)
+#define InitArr2(c,n) memset(&c[0][0],0,sizeof(int)*n)
+
+template<class T>
+bool valid(T x, T w) { return 0 <= x&&x < w; }
+
+int di[4] = { 1, -1, 0, 0 };
+int dj[4] = { 0, 0, 1, -1 };
+
+using namespace std;
+
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+
+bool s[1000000];
+bool k[1000000];
+
+signed main()
+{
+	QUICK_CIN;
+	//fstream cin("debug.txt");
+	//ofstream cout("result.txt");
+
+	int w, h, n;
+	cin >> w >> h >> n;
+
+	REP(i, n) {
+		int c;
+		cin >> c;
+		s[c - 1] = true;
+		cin >> c;
+		k[c - 1] = true;
+	}
+
+	int snum = 0;
+	for (auto x : s) {
+		if (x)++snum;
+	}
+
+	int knum = 0;
+	for (auto x : k) {
+		if (x)++knum;
+	}
+
+	cout << snum*(h - knum) + knum*w - n << endl;
+}
