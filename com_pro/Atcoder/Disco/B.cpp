787ea785ff0ece0b34d6d72d71e37e164a7be794
nobu <pyomta13@gmail.com>
2016-02-03 14:38:38 +0900

    Create B.cpp

---
+++ com_pro/Atcoder/Disco/B.cpp
@@ -0,0 +1,103 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <limits>
+#include <functional>
+#include <stack>
+#include <array>
+#include <fstream>
+#include <deque>
+#include <bitset>
+using namespace std;
+#define REP(i,n) for(int (i) = 0;(i) < (n) ; ++(i))
+#define REPS(a,i,n) for(int (i) = (a) ; (i) < (n) ; ++(i))
+#if defined(_MSC_VER)||__cplusplus > 199711L
+#define AUTO(r,v) auto r = (v)
+#else
+#define AUTO(r,v) typeof(v) r = (v)
+#endif
+#define ALL(c) (c).begin() , (c).end()
+#define EACH(it,c) for(AUTO(it,(c).begin());it != (c).end();)
+#define LL long long
+#define lint LL
+#define inf  ((int)1 << 54)
+#define mod 1000000007
+#define QUICK_CIN ios::sync_with_stdio(false); cin.tie(0);
+#define lowb lower_bound
+#define upb upper_bound
+#define ZERO(c,n) memset(&c[0],0,sizeof(int)*n)
+#define ZERO2(c,n) memset(&c[0][0],0,sizeof(int)*n)
+#define debug_input fstream cin("input.txt");ofstream cout("output.txt");
+#define pb(a) push_back(a)
+template<class T>void scan(vector<T>& a, int n, istream& cin) { T c; REP(i, n) { cin >> c; a.push_back(c); } }
+using vs = vector<string>; using vi = vector<int>; using pii = pair<int, int>; using psi = pair<string, int>; using vvi = vector<vi>;
+template<class T>bool valid(T x, T w) { return 0 <= x&&x < w; }
+int dx[4] = { 1, -1, 0, 0 }; int dy[4] = { 0, 0, 1, -1 };
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+ 
+vector<int> aa[500000];
+set<int> se;
+ 
+signed main()
+{
+	QUICK_CIN;
+	
+	int n;
+	cin >> n;
+ 
+	REP(i, n) {
+		int c;
+		cin >> c;
+		
+		int it = i;
+ 
+		if (i == 0)
+			it = n;
+ 
+		se.insert(c);
+		aa[c].push_back(it);
+	}
+ 
+	for (auto& x : se) {
+		if(aa[x].size() != 1)
+			sort(ALL(aa[x]));
+	}
+ 
+	int count = 0;
+ 
+	auto it = aa[*(se.begin())].back();
+ 
+	if (it == n) {
+		it = 0;
+	}
+ 
+	se.erase(se.begin());
+ 
+	for (auto x : se) {
+		auto s = upper_bound(ALL(aa[x]), it);
+		
+		if (s != aa[x].begin()) {
+			count++;
+			--s;
+		}
+ 
+		it = *s;
+	}
+ 
+	cout << count + 1 << endl;
+}
