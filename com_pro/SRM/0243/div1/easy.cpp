9ad0b72aeee1f315c1c690be6824726fcc15d4e2
nobu <pyomta13@gmail.com>
2016-02-03 09:35:07 +0900

    Update easy.cpp

---
+++ com_pro/SRM/0243/div1/easy.cpp
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <limits>
+#include <functional>
+#include <stack>
+#include <array>
+#include <fstream>
+#include <deque>
+#include <bitset>
+using namespace std;
+#define REP(i,n) for(int (i) = 0;(i) < (n) ; ++(i))
+#define REPS(a,i,n) for(int (i) = (a) ; (i) < (n) ; ++(i))
+#if defined(_MSC_VER)||__cplusplus > 199711L
+#define AUTO(r,v) auto r = (v)
+#else
+#define AUTO(r,v) typeof(v) r = (v)
+#endif
+#define ALL(c) (c).begin() , (c).end()
+#define EACH(it,c) for(AUTO(it,(c).begin());it != (c).end();)
+#define LL long long
+#define lint LL
+#define inf  ((int)1 << 54)
+#define mod 1000000007
+#define QUICK_CIN ios::sync_with_stdio(false); cin.tie(0);
+#define lowb lower_bound
+#define upb upper_bound
+#define ZERO(c,n) memset(&c[0],0,sizeof(int)*n)
+#define ZERO2(c,n) memset(&c[0][0],0,sizeof(int)*n)
+#define debug_input fstream cin("input.txt");ofstream cout("output.txt");
+#define pb(a) push_back(a)
+template<class T>void scan(vector<T>& a, int n, istream& cin) { T c; REP(i, n) { cin >> c; a.push_back(c); } }
+using vs = vector<string>; using vi = vector<int>; using pii = pair<int, int>; using psi = pair<string, int>; using vvi = vector<vi>;
+template<class T>bool valid(T x, T w) { return 0 <= x&&x < w; }
+int dx[4] = { 1, -1, 0, 0 }; int dy[4] = { 0, 0, 1, -1 };
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+
+
+bool ed[1000];
+int p;
+
+bool f(int k)
+{
+	REP(i, 1000) {
+		ed[i] = false;
+	}
+	ed[1] = true;
+
+	int a = k;
+
+	REP(i, p - 2) {
+		k %= p;
+		if (!ed[k])
+			ed[k] = true;
+		else
+			return false;
+
+		k *= a;
+	}
+	return true;
+}
+
+class Generators {
+public:
+	vector <int> find(int p)
+	{
+		vi a;
+
+		REP(i, p - 1) {
+			if (f(i + 1)) {
+				a.pb(i + 1);
+			}
+		}
+
+		return a;
+	}
+};
