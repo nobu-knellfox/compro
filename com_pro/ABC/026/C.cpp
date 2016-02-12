86435156c6afb09560463fd61d93d6c3e6084791
nobu <pyomta13@gmail.com>
2016-02-12 10:34:57 +0900

    Update C.cpp

---
+++ com_pro/ABC/026/C.cpp
@@ -0,0 +1,102 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <limits>
+#include <functional>
+#include <stack>
+#include <array>
+#include <fstream>
+#include <deque>
+#include <bitset>
+using namespace std;
+#define REP(i,n) for(int (i) = 0;(i) < (n) ; ++(i))
+#define REPS(a,i,n) for(int (i) = (a) ; (i) < (n) ; ++(i))
+#define REVERSE(i,n) for(int (i) = n-1;(i) >= 0 ; --i)
+#if defined(_MSC_VER)||__cplusplus > 199711L
+#define AUTO(r,v) auto r = (v)
+#else
+#define AUTO(r,v) typeof(v) r = (v)
+#endif
+#define ALL(c) (c).begin() , (c).end()
+#define EACH(it,c) for(AUTO(it,(c).begin());it != (c).end();)
+#define LL long long
+#define int LL
+#define inf  (1LL << 50)
+#define mod 1000000007
+#define QUICK_CIN ios::sync_with_stdio(false); cin.tie(0);
+#define lowb lower_bound
+#define upb upper_bound
+#define ZERO(c,n) memset(&c[0],0,sizeof(int)*n)
+#define ZERO2(c,n) memset(&c[0][0],0,sizeof(int)*n
+#define pl(a) cout << ""#a": " << a << endl;
+#ifdef _DEBUG
+#define debug_io fstream cin("input.txt");ofstream cout("output.txt");
+#else
+#define debug_io ;
+#endif
+#define debug_input debug_io
+#define pb(a) push_back(a)
+template<class T>void scan(vector<T>& a, int n, istream& cin) { T c; REP(i, n) { cin >> c; a.push_back(c); } }
+using vs = vector<string>; using vi = vector<int>; using pii = pair<int, int>; using psi = pair<string, int>; using vvi = vector<vi>;
+template<class T>bool valid(T x, T w) { return 0 <= x&&x < w; }
+int dx[4] = { 1, -1, 0, 0 }; int dy[4] = { 0, 0, 1, -1 };
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+
+
+vi a[30];
+int memo[30];
+
+int f(int i)
+{
+	if (a[i].empty()) {
+		return memo[i] = 1;
+	}
+
+	if (memo[i] > 0) {
+		return memo[i];
+	}
+
+	int mixi = inf;
+	int maxi = -inf;
+
+	for (auto x : a[i]) {
+		mixi = min(f(x), mixi);
+		maxi = max(f(x), maxi);
+	}
+	return memo[i] = mixi + maxi + 1;
+
+}
+
+signed main()
+{
+	QUICK_CIN;
+	debug_input;
+
+	int n;
+	cin >> n;
+
+	REP(i, n-1) {
+		int c;
+		cin >> c;
+		c--;
+		a[c].push_back(i+1);
+	}
+
+	cout << f(0) << endl;
+
+}
