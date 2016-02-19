8999856d5b9c3a620bb4e16695ae229b7c90a9d1
nobu <pyomta13@gmail.com>
2016-02-19 16:12:18 +0900

    Update D.cpp

---
+++ com_pro/ABC/021/D.cpp
@@ -0,0 +1,209 @@
+/**************************************
+99点解法
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <limits>
+#include <functional>
+#include <stack>
+#include <array>
+#include <fstream>
+#include <deque>
+#include <bitset>
+using namespace std;
+#define REP(i,n) for(int (i) = 0;(i) < (n) ; ++(i))
+#define REPS(a,i,n) for(int (i) = (a) ; (i) < (n) ; ++(i))
+#define REVERSE(i,n) for(int (i) = n-1;(i) >= 0 ; --i)
+#if defined(_MSC_VER)||__cplusplus > 199711L
+#define AUTO(r,v) auto r = (v)
+#else
+#define AUTO(r,v) typeof(v) r = (v)
+#endif
+#define ALL(c) (c).begin() , (c).end()
+#define EACH(it,c) for(AUTO(it,(c).begin());it != (c).end();)
+#define LL long long
+#define int LL
+#define inf  (int)(1 << 50)
+#define mod 1000000007
+#define QUICK_CIN ios::sync_with_stdio(false); cin.tie(0);
+#define lowb lower_bound
+#define upb upper_bound
+#define ZERO(c,n) memset(&c[0],0,sizeof(int)*n)
+#define ZERO2(c,n) memset(&c[0][0],0,sizeof(int)*n
+#define pl(a) cout << ""#a": " << a << endl;
+#ifdef _DEBUG
+#define debug_io fstream cin("input.txt");ofstream cout("output.txt");
+#else
+#define debug_io ;
+#endif
+#define debug_input debug_io
+#define pb(a) push_back(a)
+template<class T>void scan(vector<T>& a, int n, istream& cin) { T c; REP(i, n) { cin >> c; a.push_back(c); } }
+using vs = vector<string>; using vi = vector<int>; using pii = pair<int, int>; using psi = pair<string, int>; using vvi = vector<vi>;
+template<class T>bool valid(T x, T w) { return 0 <= x&&x < w; }
+int dx[4] = { 1, -1, 0, 0 }; int dy[4] = { 0, 0, 1, -1 };
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+
+int dp[1002][1002];
+
+int table[100001];
+
+signed main()
+{
+	QUICK_CIN;
+	debug_input;
+
+	REP(i, 1002) {
+		REP(j, 1002) {
+			dp[i][j] = 1;
+		}
+	}
+
+	int n, k;
+	cin >> n >> k;
+
+	REP(i, 1001) {
+		REP(j, 1001) {
+			dp[i + 1][j + 1] = dp[i + 1][j] + dp[i][j + 1];
+			dp[i + 1][j + 1] %= mod;
+		}
+	}
+
+	cout << dp[k][n-1] << endl;
+
+}
+*/
+
+/*
+100点解法
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <limits>
+#include <functional>
+#include <stack>
+#include <array>
+#include <fstream>
+#include <deque>
+#include <bitset>
+using namespace std;
+#define REP(i,n) for(int (i) = 0;(i) < (n) ; ++(i))
+#define REPS(a,i,n) for(int (i) = (a) ; (i) < (n) ; ++(i))
+#define REVERSE(i,n) for(int (i) = n-1;(i) >= 0 ; --i)
+#if defined(_MSC_VER)||__cplusplus > 199711L
+#define AUTO(r,v) auto r = (v)
+#else
+#define AUTO(r,v) typeof(v) r = (v)
+#endif
+#define ALL(c) (c).begin() , (c).end()
+#define EACH(it,c) for(AUTO(it,(c).begin());it != (c).end();)
+#define LL long long
+#define int LL
+#define inf  (int)(1 << 50)
+#define mod 1000000007
+#define QUICK_CIN ios::sync_with_stdio(false); cin.tie(0);
+#define lowb lower_bound
+#define upb upper_bound
+#define ZERO(c,n) memset(&c[0],0,sizeof(int)*n)
+#define ZERO2(c,n) memset(&c[0][0],0,sizeof(int)*n
+#define pl(a) cout << ""#a": " << a << endl;
+#ifdef _DEBUG
+#define debug_io fstream cin("input.txt");ofstream cout("output.txt");
+#else
+#define debug_io ;
+#endif
+#define debug_input debug_io
+#define pb(a) push_back(a)
+template<class T>void scan(vector<T>& a, int n, istream& cin) { T c; REP(i, n) { cin >> c; a.push_back(c); } }
+using vs = vector<string>; using vi = vector<int>; using pii = pair<int, int>; using psi = pair<string, int>; using vvi = vector<vi>;
+template<class T>bool valid(T x, T w) { return 0 <= x&&x < w; }
+int dx[4] = { 1, -1, 0, 0 }; int dy[4] = { 0, 0, 1, -1 };
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+
+int table[100001];
+
+
+int modpow(int a, int n)
+{
+	int k = 1;
+	while (n) {
+		k *= (n % 2 == 1 ? a : 1);
+		k %= mod;
+		n /= 2;
+		a *= a;
+		a %= mod;
+	}
+	return k;
+}
+
+signed main()
+{
+	QUICK_CIN;
+	debug_input;
+
+	int n, k;
+
+	cin >> n >> k;
+
+	int kk = 1;
+	REPS(1,i, 100001) {
+		kk *= i;
+		kk %= mod;
+	}
+
+	table[100000] = modpow(kk,mod-2);
+
+	for (int i = 100000; 0 < i; --i) {
+		table[i - 1] = table[i] * i;
+		table[i - 1] %= mod;
+	}
+
+	int s = 1;
+	REP(i, n + k - 1) {
+		s *= (i + 1);
+		s %= mod;
+	}
+
+	int ss = s * table[n - 1];
+	ss %= mod;
+	ss *= table[k];
+	ss %= mod;
+
+	cout << ss << endl;
+}
+
+
+
+
