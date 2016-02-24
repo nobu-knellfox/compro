38a86664f930a30819e5ff1a198ab3813cbbbad6
nobu <pyomta13@gmail.com>
2016-02-24 18:09:52 +0900

    Update C.cpp

--- com_pro/ABC/002/C.cpp
+++ com_pro/ABC/002/C.cpp
@@ -18,51 +18,67 @@
 #include <functional>
 #include <stack>
 #include <array>
- 
-#define REP(i,n) for(int (i) = 0;(i) < (n) ; ++(i))
-#define REPA(a,i,n) for(int (i) = (a) ; (i) < (n) ; ++(i))
-#if defined(_MSC_VER)||__cplusplus > 199711L
-#define AUTO(r,v) auto r = (v)
-#else
-#define AUTO(r,v) typeof(v) r = (v)
-#endif
-#define ALL(c) (c).begin() , (c).end()
-#define EACH(it,c) for(AUTO(it,(c).begin());it != (c).end();++it)
-#define LL long long 
-#define int long
-#define INF  99999999;
-#define QUICK_CIN ios::sync_with_stdio(false); cin.tie(0);
+#include <fstream>
+#include <deque>
+#include <bitset>
+#include <complex>
 using namespace std;
- 
-typedef pair<double, double> point;
- 
-point MakeVec(point a, point b)
+#define REP(i,n) for(int (i) = 0;(i) < (n) ; ++(i))
+#define REPS(a,i,n) for(int (i) = (a) ; (i) < (n) ; ++(i))
+#define REVERSE(i,n) for(int (i) = n-1;(i) >= 0 ; --i)
+#define ALL(c) (c).begin() , (c).end()
+#define EACH(it,c) for(auto(it,(c).begin());it != (c).end();)
+#define cx() real()
+#define cy() imag()
+#define px first
+#define py second
+#define LL long long
+#define int LL
+#define inf  (1LL << 60)
+#define mod 1000000007
+#define QUICK_CIN ios::sync_with_stdio(false); cin.tie(0);
+#define lower lower_bound
+#define upper upper_bound
+#define ZERO(c,n) memset(&c[0],0,sizeof(int)*n)
+#define ZERO2(c,n) memset(&c[0][0],0,sizeof(int)*n
+#define pl(a) cout << ""#a": " << a << endl;
+#ifdef _DEBUG
+#define debug_io fstream cin("input.txt");ofstream cout("output.txt");
+#else
+#define debug_io ;
+#endif
+#define debug_input debug_io
+#define pb(a) push_back(a)
+template<class T>void scan(vector<T>& a, int n, istream& cin)
 {
-	return point(b.first - a.first, b.second - a.second);
+	T c; REP(i, n) { cin >> c; a.push_back(c); }
 }
- 
-double CrossProduct(point a, point b, point c, point d)
-{
-	point ab = MakeVec(a, b);
-	point cd = MakeVec(c, d);
- 
-	return ab.first*cd.second - ab.second*cd.first;
-}
- 
-double TriangleArea(point a, point b, point c)
-{
-	return abs(CrossProduct(a, b, a, c) / 2);
-}
- 
+using vs = vector<string>; using vi = vector<int>; using pii = pair<int, int>;
+using psi = pair<string, int>; using vvi = vector<vi>; using pss = pair<string, string>;
+template<class T>bool valid(T x, T w) { return 0 <= x&&x < w; }
+int dx[4] = { 1, -1, 0, 0 }; int dy[4] = { 0, 0, 1, -1 };
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+
+using cdd = complex<double>;
+
 signed main()
 {
-	//QUICK_CIN;
- 
-	point a, b, c;
- 
-	cin >> a.first >> a.second >> b.first >> b.second >> c.first >> c.second;
- 
-	printf("%.2lf\n", TriangleArea(a, b, c));
- 
+	QUICK_CIN;
+	debug_input;
+
+	double ax, ay, bx, by, cx, cy;
+	cin >> ax >> ay >> bx >> by >> cx >> cy;
+	
+	cdd a(ax,ay), b(bx,by), c(cx,cy);
+
+	cdd ab = b - a;
+	cdd ac = c - a;
+
+	cout.precision(10);
+
+	cout << abs((ab*conj(ac)).cy()/2) << endl;
+
 	return 0;
 }
