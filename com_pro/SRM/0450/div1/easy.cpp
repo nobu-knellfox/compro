06a597871c25ca3a8ba0b136cf482aec7d773f98
nobu <pyomta13@gmail.com>
2016-03-02 09:23:36 +0900

    Update easy.cpp

--- com_pro/SRM/0450/div1/easy.cpp
+++ com_pro/SRM/0450/div1/easy.cpp
@@ -24,6 +24,7 @@
 using namespace std;
 #define REP(i,n) for(int (i) = 0;(i) < (n) ; ++(i))
 #define REPS(a,i,n) for(int (i) = (a) ; (i) < (n) ; ++(i))
+#define REVERSE(i,n) for(int (i) = n-1;(i) >= 0 ; --i)
 #if defined(_MSC_VER)||__cplusplus > 199711L
 #define AUTO(r,v) auto r = (v)
 #else
@@ -31,54 +32,55 @@ using namespace std;
 #endif
 #define ALL(c) (c).begin() , (c).end()
 #define EACH(it,c) for(AUTO(it,(c).begin());it != (c).end();)
+#define cx() real()
+#define cy() imag()
+#define px first
+#define py second
 #define LL long long
 #define lint LL
-#define inf  ((int)1 << 54)
+#define inf  ((int)1 << 30)
 #define mod 1000000007
 #define QUICK_CIN ios::sync_with_stdio(false); cin.tie(0);
 #define lowb lower_bound
 #define upb upper_bound
 #define ZERO(c,n) memset(&c[0],0,sizeof(int)*n)
 #define ZERO2(c,n) memset(&c[0][0],0,sizeof(int)*n)
-#define debug_input fstream cin("input.txt");ofstream cout("output.txt");
+#define pl(a) cout << ""#a": " << a << endl;
+#ifdef _DEBUG
+#define debug_io fstream cin("input.txt");ofstream cout("output.txt");
+#else
+#define debug_io ;
+#endif
+#define debug_input debug_io
 #define pb(a) push_back(a)
 template<class T>void scan(vector<T>& a, int n, istream& cin) { T c; REP(i, n) { cin >> c; a.push_back(c); } }
 using vs = vector<string>; using vi = vector<int>; using pii = pair<int, int>; using psi = pair<string, int>; using vvi = vector<vi>;
 template<class T>bool valid(T x, T w) { return 0 <= x&&x < w; }
 int dx[4] = { 1, -1, 0, 0 }; int dy[4] = { 0, 0, 1, -1 };
-//-----------------------------------------------------------------------------------------------
-//-----------------------------------------------------------------------------------------------
-//-----------------------------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
 
 
 class OrderedNim {
 public:
-	string winner(vector <int> layout)
-	{
-		int k = 0;
-		vi a = layout;
+	string winner(vector <int> layout){
 
-		bool b = false;
+		vi a = layout;
+		bool f = true;
 
 		a.pop_back();
-
-		if (a.empty()) {
-			return "Alice";
-		}
-
-		int it = 0;
-		b = a[it] == 1;
-		++it;
-
-		for (; it < a.size(); ++it) {
-			if (a[it] == 1) {
-				if (a[it - 1] < 2) {
-					b != b;
-				}
+		for (auto x : a) {
+			if (x == 1) {
+				f = !f;
+			}
+			else {
+				break;
 			}
 		}
 
-		if (!b) {
+		if (f) {
 			return "Alice";
 		}
 		else {
@@ -86,3 +88,4 @@ public:
 		}
 	}
 };
+
