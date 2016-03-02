6ca7b273dcbcae3a64b02e9f336eab7412a7c3a3
nobu <pyomta13@gmail.com>
2016-03-02 09:07:00 +0900

    Update easy.cpp

---
+++ com_pro/SRM/0446/div1/easy.cpp
@@ -0,0 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <limits>
+#include <functional>
+#include <stack>
+#include <array>
+#include <fstream>
+#include <deque>
+#include <bitset>
+using namespace std;
+#define REP(i,n) for(int (i) = 0;(i) < (n) ; ++(i))
+#define REPS(a,i,n) for(int (i) = (a) ; (i) < (n) ; ++(i))
+#define REVERSE(i,n) for(int (i) = n-1;(i) >= 0 ; --i)
+#if defined(_MSC_VER)||__cplusplus > 199711L
+#define AUTO(r,v) auto r = (v)
+#else
+#define AUTO(r,v) typeof(v) r = (v)
+#endif
+#define ALL(c) (c).begin() , (c).end()
+#define EACH(it,c) for(AUTO(it,(c).begin());it != (c).end();)
+#define cx() real()
+#define cy() imag()
+#define px first
+#define py second
+#define LL long long
+#define lint LL
+#define inf  ((int)1 << 30)
+#define mod 1000000007
+#define QUICK_CIN ios::sync_with_stdio(false); cin.tie(0);
+#define lowb lower_bound
+#define upb upper_bound
+#define ZERO(c,n) memset(&c[0],0,sizeof(int)*n)
+#define ZERO2(c,n) memset(&c[0][0],0,sizeof(int)*n)
+#define pl(a) cout << ""#a": " << a << endl;
+#ifdef _DEBUG
+#define debug_io fstream cin("input.txt");ofstream cout("output.txt");
+#else
+#define debug_io ;
+#endif
+#define debug_input debug_io
+#define pb(a) push_back(a)
+template<class T>void scan(vector<T>& a, int n, istream& cin) { T c; REP(i, n) { cin >> c; a.push_back(c); } }
+using vs = vector<string>; using vi = vector<int>; using pii = pair<int, int>; using psi = pair<string, int>; using vvi = vector<vi>;
+template<class T>bool valid(T x, T w) { return 0 <= x&&x < w; }
+int dx[4] = { 1, -1, 0, 0 }; int dy[4] = { 0, 0, 1, -1 };
+////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+
+int cube[3][3] =
+{ 0,1,0,
+	1,2,1,
+	0,1,0 };
+
+string ans[3] = { "RED","BLUE","GREEN" };
+
+class CubeWalking {
+public:
+	string finalPosition(string movement) {
+		int di, dj;
+		di = 0;
+		dj = 1;
+
+		int nowi, nowj;
+		nowi = 1;
+		nowj = 1;
+
+		string cmd = movement;
+
+		for (auto x : cmd) {
+			if (x == 'L') {
+				int ti = -1 * dj;
+				int tj = di;
+				di = ti;
+				dj = tj;
+			}
+			else if (x == 'R') {
+				int ti = dj;
+				int tj = -1 * di;
+				di = ti;
+				dj = tj;
+			}
+			else if (x == 'W') {
+				nowi += 3 + di;
+				nowj += 3 + dj;
+				nowi %= 3;
+				nowj %= 3;
+			}
+		}
+
+		return ans[cube[nowi][nowj]];
+	}
+};
+
